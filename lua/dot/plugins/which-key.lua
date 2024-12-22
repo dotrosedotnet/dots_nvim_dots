cd827f66487c2333b55d32385a11d54b7f79bf83
dotrosedotnet <stuff@dottie.me>
2024-12-22 10:30:22 -0500

    fix local redefinition of 'opts'

--- lua/dot/plugins/which-key.lua
+++ lua/dot/plugins/which-key.lua
@@ -48,10 +48,10 @@ return {
 			local function map(...)
 				vim.api.nvim_buf_set_keymap(0, ...)
 			end
-			local opts = { noremap = true, silent = false }
+			local options = { noremap = true, silent = false }
 
 			-- Open the link under the caret.
-			map("n", "<CR>", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+			map("n", "<CR>", "<Cmd>lua vim.lsp.buf.definition()<CR>", options)
 
 			-- Create a new note after asking for its title.
 			-- This overrides the global `<leader>zn` mapping to create the note in the same directory as the current buffer.
@@ -59,29 +59,29 @@ return {
 				"n",
 				"<leader>zn",
 				"<Cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>",
-				opts
+				options
 			)
 			-- Create a new note in the same directory as the current buffer, using the current selection for title.
-			map("v", "<leader>znt", ":'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>", opts)
+			map("v", "<leader>znt", ":'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>", options)
 			-- Create a new note in the same directory as the current buffer, using the current selection for note content and asking for its title.
 			map(
 				"v",
 				"<leader>znc",
 				":'<,'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>",
-				opts
+				options
 			)
 
 			-- Open notes linking to the current buffer.
-			map("n", "<leader>zb", "<Cmd>ZkBacklinks<CR>", opts)
+			map("n", "<leader>zb", "<Cmd>ZkBacklinks<CR>", options)
 			-- Alternative for backlinks using pure LSP and showing the source context.
-			--map('n', '<leader>zb', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+			--map('n', '<leader>zb', '<Cmd>lua vim.lsp.buf.references()<CR>', options)
 			-- Open notes linked by the current buffer.
-			map("n", "<leader>zl", "<Cmd>ZkLinks<CR>", opts)
+			map("n", "<leader>zl", "<Cmd>ZkLinks<CR>", options)
 
 			-- Preview a linked note.
-			map("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+			map("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", options)
 			-- Open the code actions for a visual selection.
-			map("v", "<leader>za", ":'<,'>lua vim.lsp.buf.range_code_action()<CR>", opts)
+			map("v", "<leader>za", ":'<,'>lua vim.lsp.buf.range_code_action()<CR>", options)
 		end
 	end,
 	setup = function()
