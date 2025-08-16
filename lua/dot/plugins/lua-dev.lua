8560478bdfb1bfc0df962c078329d882d15c35d1
dotrosedotnet <stuff@dottie.me>
2025-08-16 07:52:25 -0400

    overdue plugin commit

---
+++ lua/dot/plugins/lua-dev.lua
@@ -0,0 +1,3 @@
+return {
+	"folke/lua-dev.nvim",
+}
---
+++ lua/dot/plugins/over_the_moon.lua
@@ -0,0 +1,7 @@
+return {
+	dir = "~/dev/neovim/over_the_moon.nvim",
+	name = "over_the_moon",
+	config = function()
+		require("over_the_moon")
+	end,
+}
