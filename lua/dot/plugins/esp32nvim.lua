3268cb93a96d1c1674bb392756b70e9c3302f5fb
dotrosedotnet <stuff@dottie.me>
2025-10-22 08:44:52 -0400

    esp32

--- lua/dot/plugins/esp32nvim.lua
+++ lua/dot/plugins/esp32nvim.lua
@@ -1,3 +1,10 @@
 return {
 	"Aietes/esp32.nvim",
+	dependencies = { "folke/snacks.nvim" },
+	lazy = false,  -- Load immediately, don't wait for ft
+	config = function()
+		require("esp32").setup({
+			build_dir = "build.clang",  -- Required: tells esp32.nvim where compile_commands.json is
+		})
+	end,
 }
