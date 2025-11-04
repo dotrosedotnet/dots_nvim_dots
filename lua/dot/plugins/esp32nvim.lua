90246fd05a5edf414597fceb441efae09287f5e6
dotrosedotnet <stuff@dottie.me>
2025-11-04 10:14:40 -0500

    make esp32 lazy

--- lua/dot/plugins/esp32nvim.lua
+++ lua/dot/plugins/esp32nvim.lua
@@ -1,10 +1,10 @@
 return {
 	"Aietes/esp32.nvim",
 	dependencies = { "folke/snacks.nvim" },
-	lazy = false,  -- Load immediately, don't wait for ft
+	lazy = true, -- Load immediately, don't wait for ft
 	config = function()
 		require("esp32").setup({
-			build_dir = "build.clang",  -- Required: tells esp32.nvim where compile_commands.json is
+			build_dir = "build.clang", -- Required: tells esp32.nvim where compile_commands.json is
 		})
 	end,
 }
